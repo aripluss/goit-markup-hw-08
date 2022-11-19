af63a7989e85bfc46e6dc135144fcf67b0954640
Iryna Pomeshkina <aripluss@i.ua>
2022-11-19 19:09:18 +0200

    Add adaptive mobile-menu

--- menu.js
+++ menu.js
@@ -1,20 +1,15 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector(‘[data-menu-open]‘),
-    closeMenuBtn: document.querySelector(‘[data-menu-close]‘),
-    menu: document.querySelector(‘[data-menu]’),
-    body: document.querySelector(‘body’),
-    menuList: document.querySelector(‘.mob-list’),
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+    body: document.querySelector('body'),
   };
-  refs.openMenuBtn.addEventListener(‘click’, toggleMenu);
-  refs.closeMenuBtn.addEventListener(‘click’, toggleMenu);
-  refs.menuList.addEventListener(‘click’, removeMenu);
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
   function toggleMenu() {
-    refs.menu.classList.toggle(‘is-hidden’);
-    refs.body.classList.toggle(‘no-scroll’);
-  }
-  function removeMenu() {
-    refs.menu.classList.add(‘is-hidden’);
-    refs.body.classList.remove(‘no-scroll’);
+    refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
   }
 })();
